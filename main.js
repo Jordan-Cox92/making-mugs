e6cb7ea658e677e1e09618e5d3b2959c546d50c9
Jordan-Cox92! <mrjordancolbycox@gmail.com>
2022-07-13 11:58:29 -0400

    mugs created

---
+++ main.js
@@ -0,0 +1,10 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const fire of clay) {
+      const mug = `${fire} coffee mug.`
+      toFireInKiln.push(mug);
+         
+}     
+
+console.log(toFireInKiln)
